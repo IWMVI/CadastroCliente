6b328d21309d0a398122dd11d537de04a94baaf1
Wallace Martins <wmvwallace@gmail.com>
2025-05-23 00:38:06 -0300

    feat: Adiciona arquivos de configuração do Maven e estrutura inicial do projeto Java Swing com Spring Boot

---
+++ src/main/java/com/javafx/javafxv1/Javafxv1Application.java
@@ -0,0 +1,24 @@
+package com.javafx.javafxv1;
+
+import com.javafx.javafxv1.ui.ClienteForm;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Javafxv1Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                new SpringApplicationBuilder(Javafxv1Application.class)
+                        .headless(false)
+                        .run(args);
+
+        SwingUtilities.invokeLater(() -> {
+            ClienteForm form = context.getBean(ClienteForm.class);
+            form.setVisible(true);
+        });
+    }
+}
